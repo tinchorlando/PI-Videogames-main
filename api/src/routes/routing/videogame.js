b96f6905615da3d1e41c76d15688e8add8da221d
tinchorlando <martin._orlando@hotmail.com.ar>
2022-06-11 02:49:55 -0300

    fixed db

--- api/src/routes/routing/videogame.js
+++ api/src/routes/routing/videogame.js
@@ -5,7 +5,8 @@ const { getOneApi , getOneDb } = require('./utils/utils.js')
 app.get('/:idVideogame',async(req,res,next)=>{
     try{
         const {idVideogame} = req.params;
-        if (parseInt(idVideogame)){
+        const regexp = ".*[a-zA-Z].*" //expresion regular que matchea si dentro del string hay una letra
+        if (!idVideogame.match(regexp)){
             res.status(200).json(await getOneApi(idVideogame))
         }
         else res.status(200).json(await getOneDb(idVideogame))
